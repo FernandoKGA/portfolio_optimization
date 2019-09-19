386b09a20ae43de5bf624ea767d9e243ee2b6bdb
FernandoKGA <fernando.kga@gmail.com>
2019-09-18 23:07:29 -0300

    Zip extractor in Julia, prep_files.py modified

--- Scripts/prep_files.py
+++ Scripts/prep_files.py
@@ -1,5 +1,5 @@
 import os
 
-os.system("python3 zip_extractor.py")
+os.system("julia zip_extractor.jl")
 os.system("python3 file_name_changer.py")
 os.system("julia imports.jl")
---
+++ Scripts/zip_extractor.jl
@@ -0,0 +1,79 @@
+if Sys.islinux()
+    root_dir = dirname(pwd())
+    file_prefix = "COTAHIST_A"
+    file_begin_counter = 1995
+    file_end_counter = 2019
+
+    path_sufix = "/Datasets/Dataset_Bovespa/"
+	path_sender = "Zipped/"
+	path_destiny = "Unzipped/"
+	
+	location = root_dir * path_sufix * path_sender
+	destiny = root_dir * path_sufix * path_destiny
+
+    println(location)
+    println(destiny)
+
+    if !isdir(destiny)
+        mkdir(destiny)
+    else
+        rm(destiny, recursive=true)
+        mkdir(destiny)
+    end
+
+    for year in range(file_begin_counter, stop=file_end_counter)
+        path_sender_with_year = [location * file_prefix * string(year)]
+        path_destiny_with_year = [destiny]
+        full_command = `unzip $path_sender_with_year -d $path_destiny_with_year`
+        run(full_command)
+    end
+else
+    if Sys.iswindows()
+        root_dir = dirname(pwd())
+        file_prefix = "COTAHIST_A"
+        file_begin_counter = 1995
+        file_end_counter = 2019
+        fileExtension = '.zip'
+    
+        path_sufix = "\\Datasets\\Dataset_Bovespa\\"
+        path_sender = "Zipped\\"
+        path_destiny = "Unzipped\\"
+        
+        location = root_dir * path_sufix * path_sender
+        destiny = root_dir * path_sufix * path_destiny
+    
+        println(location)
+        println(destiny)
+    
+        if !isdir(destiny)
+            mkdir(destiny)
+        else
+            rm(destiny, recursive=true)
+            mkdir(destiny)
+        end
+    
+        #=
+        powerShellCommand = 'Expand-Archive -Force '
+
+	    for year in range(fileStartYear, fileEndYear + 1):
+		    completeCommand = powerShellCommand + location + filePrefix + str(year) + fileExtension + ' ' + destiny
+		    print(completeCommand)
+		    result = subprocess.Popen(['powershell.exe',completeCommand])
+		    print(result)
+        print("Done.")
+        =#
+
+        #=
+        for year in range(file_begin_counter, stop=file_end_counter)
+            path_sender_with_year = [location * file_prefix * string(year)]
+            path_destiny_with_year = [destiny]
+            full_command = `unzip $path_sender_with_year -d $path_destiny_with_year`
+            run(full_command)
+        end 
+        =#
+    else
+        println("System not implemented")
+    end
+end
+
+println("Done.")
